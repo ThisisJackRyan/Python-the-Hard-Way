a2ea90b0f06059fbccd373671fd6a5cfa1f4c0a9
Jack Ryan <jack@canopyteam.org>
2023-11-27 13:57:13 -0600

    Exercuse 42

--- main.py
+++ main.py
@@ -1,75 +1,51 @@
-from sys import exit
+class Animal(object):
 
-def gold_room():
-    print("this room is full of gold. how muyuch do you take")
+    pass
 
-    choice = input("> ")
-    if "0" in choice or "1" in choice:
-        how_much  =int(choice)
-    else:
-        dead("Man, learn to type a number")
-    if how_much < 50:
-        print("Nice, you're not greed, you win!")
-        exit(0)
-    else:
-        dead("You greedy boi")
+class Dog((Animal)):
+
+    def __init__(self, name):
+        self.name = name
+
+class Cat(Animal):
+
+    def __init__(self, name):
+        self.name = name
+
+class Person(object):
+
+    def __init__(self, name):
+        self.name = name
+        self.pet = None
+
     
+class Employee(Person):
 
-def bear_room():
-    print("there is a bear here .")
-    print("the bear has a bunch of honey")
-    print("The fat bear is in front of another door. ")
-    print("How are you going to moe the bear?")
-    bear_moved = False
+    def __init__(self, name, salary):
+        super(Employee, self).__init__(name)
+        self.salary = salary
 
-    while True:
-        choice = input("> ")
-        
-        if choice == "take honey":
-            dead("The bear looks at you then slaps your face off")
-        elif choice == "taunt bear" and not bear_moved:
-            print("the bear has moved from the door.")
-            print("you can go through it now")
-            bear_moved = True
-        elif choice == "taunt bear" and bear_moved:
-            dead("The bear in fact chewed your leg off")
-        elif choice == "open door" and bear_moved:
-            gold_room()
-        else:
-            print("I got no idea what that means")
-            
-        
+class fish(object):
+    pass
 
-def cthulhu_room():
-    print("here you see the great evil Cthulhu. ")
-    print("he, it , whatever stares at yu and you go insane")
-    print("Do you flee for your life or eat your head?")
-    
-    choice = input("> ")
-    if "flee" in choice:
-        start()
-    elif "head" in choice:
-        dead("well that was Tasty!")
-    else:
-        cthulhu_room()
-
-def dead(why):
-    print(why, "Good Job!")
-    exit(0)
-
-def start():
-    print("You are in a dark room.")
-    print("There is a door to your right and left")
-    print("which one do you take?")
-
-    choice = input("> ")
-
-    if choice == "left":
-        bear_room()
-    elif choice == "right":
-        cthulhu_room()
-    else:
-        dead("You stumble around till you starve.")
+class Salmon(fish):
+    pass
+class Halibut(fish):
+    pass
 
 
-start()
+rover = Dog("Rover")
+satan = Cat("Satan")
+mary = Dog("Mary")
+
+mary.pet = satan
+
+frank = Employee("Frank", 120000)
+
+frank.pet = rover
+
+flipper = fish()
+
+crouse = Salmon()
+
+harry = Halibut()
