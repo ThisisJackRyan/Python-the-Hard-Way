82df2d9de9ebd17d6b6f0e20bba70b0139afc521
Jack Ryan <jack@canopyteam.org>
2023-11-27 14:15:47 -0600

    messing around with ex44

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 class Parent(object):
-    
+
+    def __init__(self, name):
+        self.name = name 
     def altered(self):
-        print("Parent Altered()")
+        print(f"Parent Altered() {self.name}")
 
 class Child(Parent):
-
+    def __init__(self, name):
+        self.name = name 
     def altered(self):
-        print("child, before parent altered()")
+        print(f"child, before parent altered() {self.name}")
         super(Child, self).altered()
-        print("Child, after Parent altered()")
+        print(f"Child, after Parent altered() {self.name}")
 
 
-dad = Parent()
-son = Child()
+dad = Parent("John")
+son = Child("Jack")
 
 dad.altered()
 son.altered()
