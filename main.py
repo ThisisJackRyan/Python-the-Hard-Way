ce6091215c1cc4c3298e9bf4f3bbe129d8fdd37c
Jack Ryan <jack@canopyteam.org>
2023-11-27 14:43:07 -0600

    Exercise 49

--- main.py
+++ main.py
@@ -1,21 +1,82 @@
-class Parent(object):
+class ParserError(Exception):
+    pass
 
-    def __init__(self, name):
-        self.name = name 
-    def altered(self):
-        print(f"Parent Altered() {self.name}")
+class Sentence(object):
 
-class Child(Parent):
-    def __init__(self, name):
-        self.name = name 
-    def altered(self):
-        print(f"child, before parent altered() {self.name}")
-        super(Child, self).altered()
-        print(f"Child, after Parent altered() {self.name}")
+    def __init__(self, subject, verb, obj):
+        self.subject = subject[1]
+        self.verb = verb[1]
+        self.object = obj[1]
+
+def peek(word_list):
+    if word_list:
+        word = word_list[0]
+        return word[0]
+    else:
+        return None
+        
+def match(word_list, expecting):
+    if word_list:
+        word = word_list.pop(0)
+
+        if word[0] == expecting:
+            return word
+        else: 
+            return None
+        
+    else: 
+        return None
+    
+def skip(word_list, word_type):
+    while peek(word_list) == word_type:
+        match(word_list, word_type)
+
+def parse_verb(word_list):
+    skip(word_list, "stop")
+
+    if peek(word_list) == "verb":
+        return match(word_list, "verb")
+    else:
+        raise ParserError("Expected a verb next")
+    
+def parse_object(word_list):
+    skip(word_list, "stop")
+    next_word = peek(word_list)
+
+    if next_word == "noun":
+        return match(word_list, "noun")
+    elif next_word == "direction":
+        return match(word_list, "direction")
+    else:
+        raise ParserError("Expected Noun or Direction next")
+    
+def parse_subject(word_list):
+    skip(word_list, "stop")
+    next_word = peek(word_list)
+
+    if next_word == "noun":
+        return match(word_list, "noun")
+    elif next_word == "verb":
+        return ("noun", "player")
+    else:
+        raise ParserError("Expected a verb next")
 
 
-dad = Parent("John")
-son = Child("Jack")
+def parse_sentence(word_list):
+    subj = parse_subject(word_list)
+    verb = parse_verb(word_list)
+    obj = parse_object(word_list)
 
-dad.altered()
-son.altered()
+    return Sentence(subj, verb, obj)
+
+
+
+x = parse_sentence([("verb","run"),("direction","north") ])
+print(x.subject)
+print(x.verb)
+print(x.object)
+
+x = parse_sentence([("noun", "bear"), ("verb", "eat"), ("stop", "the"), ("noun", "honey")])
+print(x.subject)
+print(x.verb)
+print(x.object)
